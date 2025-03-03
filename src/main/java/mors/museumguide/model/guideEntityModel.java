acf80322466bde02258eda98116e674050248013
moritzschroeter <106035175+moritzschroeter@users.noreply.github.com>
2025-03-03 11:37:35 +0100

    update guide entity model dimensions and texture size

--- src/main/java/mors/museumguide/model/guideEntityModel.java
+++ src/main/java/mors/museumguide/model/guideEntityModel.java
@@ -1,11 +1,9 @@
 package mors.museumguide.model;
 
-import mors.museumguide.entity.guideEntity;
 import mors.museumguide.state.guideEntityRenderState;
 import net.minecraft.client.model.*;
 import net.minecraft.client.render.entity.model.EntityModel;
 import net.minecraft.client.render.entity.model.EntityModelPartNames;
-import net.minecraft.util.math.MathHelper;
 
 public class guideEntityModel extends EntityModel<guideEntityRenderState> {
     private final ModelPart body;
@@ -20,8 +18,8 @@ public class guideEntityModel extends EntityModel<guideEntityRenderState> {
         ModelPartData root = modelData.getRoot();
         root.addChild(
                 EntityModelPartNames.BODY
-                , ModelPartBuilder.create().uv(-16, -8).cuboid(-5.0F, -21.0F, -5.0F, 10.0F, 21.0F, 10.0F, new Dilation(0.0F)), ModelTransform.pivot(0.0F, 24.0F, 0.0F));
-        return TexturedModelData.of(modelData, 16, 16);
+                ,ModelPartBuilder.create().uv(0, 0).cuboid(-6.0F, -24.0F, -6.0F, 12.0F, 24.0F, 12.0F, new Dilation(0.0F)), ModelTransform.pivot(0.0F, 24.0F, 0.0F));
+        return TexturedModelData.of(modelData, 64, 64);
     }
     @Override
     public void setAngles(guideEntityRenderState state) {
